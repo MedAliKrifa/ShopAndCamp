381fd6e806b3c15dc20a59e423b14b4183cf9f56
MedAliKrifa <daliolimona@gmail.com>
2023-02-16 10:33:33 +0100

    shop and camp

---
+++ Shop and CampFrontEnd/src/data.js
@@ -0,0 +1,75 @@
+export const sliderItems = [
+ {
+    id: 1,
+    img : "https://contents.mediadecathlon.com/p2011517/k$d46b4c4ab03b611e25c1c4837289523c/1180x0/2042pt2036/3614xcr4016/reparer-arceaux-tente-arpenaz-3-personnes-fresh-and-black-quechua-cassee.png?format=auto&quality=80",
+    title: "Black nd White TENT",
+    desc: "Best Tent for all time! Don't compare it to any other",
+    bg: "f5fafd", 
+ },
+  {
+    id: 2,
+    img : "https://i.pinimg.com/originals/a0/d5/75/a0d5756a27bad088708401b4c2d164dd.png",
+    title: "Classic TENT",
+    desc: "Best Tent for all time! Don't compare it to any other",
+    bg: "fcf1ed", 
+ },
+   {
+    id: 3,
+    img : "https://media-www.canadiantire.ca/product/playing/camping/backpacks-luggage-accessories/0763725/outbound-canyon-65l-backpack-537c6f09-e166-469c-b4d7-9eb8220c84de.png",
+    title: "Camping BackPack",
+    desc: "Best BackPAck for all time! Don't compare it to any other",
+    bg: "fcf1ed", 
+ }
+]
+export const categories = [
+ {
+    id: 1,
+    img : "https://images.pexels.com/photos/756780/pexels-photo-756780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Tents", 
+ },
+  {
+    id: 2,
+    img : "https://images.pexels.com/photos/10762614/pexels-photo-10762614.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Camping BackPack",
+ },
+   {
+    id: 3,
+    img : "https://images.pexels.com/photos/9615801/pexels-photo-9615801.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Camping BackPack",
+ }
+]
+export const popularProducts = [
+ {
+    id: 1,
+    img : "https://lespetitsmontagnards.com/wp-content/uploads/2D8F85DD-8FB0-43D1-95D3-6C34705B985E.png",
+ },
+  {
+    id: 2,
+    img : "https://lzd-img-global.slatic.net/g/p/4deeed330f7935631d1262842c7b9348.png_720x720q80.png_.webp",
+ },
+   {
+    id: 3,
+    img : "https://contents.mediadecathlon.com/p1897283/k$fbc89180f010291fc80dc4ea9c0335a5",
+ },
+   {
+    id: 4,
+    img : "https://freepngimg.com/thumb/backpack/143422-backpack-camping-sports-free-download-image.png",
+ },
+   {
+    id: 5,
+    img : "https://cdn.hskcz.cz/temp/img/ex/expedition-backpack-turistika-sloper-45-l-w570-h640-e-75464096e044b6d792d7fc13dd1854f6.png?vshcdn-webp-noautconvert=1",
+ },
+   {
+    id: 6,
+    img : "https://www.coghlans.com/product/image/large/8814_1.png",
+ },
+   {
+    id: 7,
+    img : "https://www.phdesigns.co.uk/images/minimus-degree-400-down-sleeping-bag/2712/800x800/minimus-degree-400-down-sleeping-bag.webp",
+ }
+ ,
+   {
+    id: 8,
+    img : "https://www.phdesigns.co.uk/images/minimus-degree-400-down-sleeping-bag/2712/800x800/minimus-degree-400-down-sleeping-bag.webp",
+ }
+]
